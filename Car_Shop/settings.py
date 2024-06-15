512fb3d8acca1e274384b7eec32963b9e4692961
Hasib Mohammad Mahede Hasan <hasib9963@gmail.com>
2024-06-15 10:09:41 +0600

    allow host

--- Car_Shop/settings.py
+++ Car_Shop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://car-shop-vjeg.onrender.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://cars-shop-ddhv.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
